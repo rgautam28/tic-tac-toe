a25a3b5ee270031f86b206ca9f22d7cf45e991db
Rakshit Gautam <rakshitgautam01@gmail.com>
2021-04-25 03:50:55 +0530

    calculate the winner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import Board from "./Components/Board"
+import { calculateWinner } from "./helpers";
 import './Style/root.scss'
 
 const App = () => {
+  const [history, setHistory] = useState([ { board: Array(9).fill(null), isXNext: true}]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+  
+  const winner = calculateWinner(current.board);
+  console.log(winner);
+
+  const message = winner ? `winner is ${winner}`: `Next player is ${current.isXNext ? 'X' : 'O'}`
+
+    console.log(history);
+
+    const handleSquareClick = (position) =>{
+        if(current.board[position] || winner){
+            return;
+        }
+        
+        setHistory( (prev) => {
+          const last= prev[prev.length-1]
+
+
+
+            const newBoard = last.board.map((Square, pos) => {
+                if(pos == position){
+                    return last.isXNext ? 'X' : 'O';
+                }
+                return Square
+            });
+            return prev.concat({board: newBoard, isXNext: !last.isXNext})
+        });
+        setCurrentMove(prev => prev+1);
+    };
+
   return  (
   <div className="app">
     <h1>Tic Tac Toe</h1>
     <small>by: Rakshit Gautam</small>
-    <Board />
+    <h2>{message}</h2>
+    <Board board={current.board} handleSquareClick={handleSquareClick} />
   </div>
   )
 };
